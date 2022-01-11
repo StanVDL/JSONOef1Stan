c143b1ab6d2e92402f206849182d357e34ae7276
StanVDL <92679342+StanVDL@users.noreply.github.com>
2022-01-11 15:20:51 +0100

    Initial commit

---
+++ Leerling.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONOef1Stan
+{
+    class Leerling
+    {
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+        public string Klas { get; set; }
+    }
+}
